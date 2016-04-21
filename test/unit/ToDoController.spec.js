24d92c43a9f8dc1b8d9c1013c90b233e897b8e50
Simon Glancy <simonglancy@Simons-MacBook-Pro-3.local>
2016-04-21 11:54:53 +0100

    first commit

---
+++ test/unit/ToDoController.spec.js
@@ -0,0 +1,14 @@
+describe('ToDoController', function() {
+  beforeEach(module('toDoApp'));
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ToDoController');
+  }));
+
+  it('initialises with a toDo', function() {
+    expect(ctrl.todo).toEqual("ToDo1");
+  });
+
+
+});
