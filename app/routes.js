28e469e4fddff78a0a7c7c228f10d8f998713fbe
Shane Oston Stowe <shane@shaneoston.com>
2016-04-22 09:47:31 +0100

    fixed issue with routes - working json

--- app/routes.js
+++ app/routes.js
@@ -3,12 +3,38 @@ var UserInfo = require('../src/userinfo.js');
 module.exports = function(app) {
 
   app.get("/api/userinfo", function(req, res) {
-    Userinfo.find(function(err, userinfo) {
-      if (err)
-        res.send(err);
+    // Userinfo.find(function(err, userinfo) {
+    //   if (err)
+    //     res.send(err);
 
-      res.json(userinfo);
-    });
+    var request = require('request');
+
+    var options = {
+      url: 'https://api.github.com/users/MisaOgura',
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        parsedData(info);
+      }
+    }
+    request(options, callback);
+
+    function parsedData(info) {
+      var frontendData = {};
+      frontendData.login = info.login;
+      frontendData.avatar_url = info.avatar_url;
+      frontendData.url = info.html_url;
+      frontendData.login = info.login;
+      frontendData.repos = info.public_repos;
+      res.json(frontendData);
+    }
+
+
   });
 
   app.get('/', function(req, res) {
