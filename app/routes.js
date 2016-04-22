51c89dce7cd83477c14ca76eb8397bcd01738cdc
Shane Oston Stowe <shane@shaneoston.com>
2016-04-22 09:00:19 +0100

    working on userinfo.js

--- app/routes.js
+++ app/routes.js
@@ -2,14 +2,20 @@ var UserInfo = require('../src/userinfo.js');
 
 module.exports = function(app) {
 
-  app.get("/api/userinfo", function(req, res) {
-    Userinfo.find(function(err, userinfo) {
-      if (err)
-        res.send(err);
+  app.get("/api/userinfo", function(req, res, username) {
+    var userinfo = new UserInfo();
 
-      res.json(userinfo);
-    });
+    var githubResponse = userinfo.requestToGitHub(username);
+
+    if (!!gitHubResponse.message) {
+      res.send(err);
+    }
+    else {
+      res.json(gitHubResponse);
+      console.log(gitHubResponse);
+    }
   });
+
 
   app.get('/', function(req, res) {
     var path = require('path');
